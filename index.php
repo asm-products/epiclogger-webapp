f06bc7103d3f854499c227e65694e2bf25a6e34d
Razvan Ciocanel <chocksy@gmail.com>
2015-05-05 12:36:24 +0300

    landing page added

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="x-ua-compatible" content="ie=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="Engage developers with users to solve errors" />
+		<title>Epic Logger</title>
+		
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:400,500" />
+		<link rel="stylesheet" type="text/css" href="assets/css/default.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/landing.css" />
+
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div id="header">
+			<div class="container">
+				<h1>Epic Logger</h1>
+
+				<h2>Fixing errors while keeping users up to date</h2>
+				<p class="tagline">You’re notified as soon as a solution has been found</p>
+				
+				<p class="announcement">We're currently hard at work developing Epic Logger on <a href="https://assembly.com/epiclogger" target="_blank">Assembly</a>. Join us there or leave your e-mail address below to be notified when we launch.</p>
+
+				<form action="http://epiclogger.us10.list-manage.com/subscribe/post?u=363b0e3a536f5a9dad6532d40&amp;id=d269d60ed8" method="post" target="_blank">
+          <div style="position: absolute; left: -5000px;"><input type="text" name="b_363b0e3a536f5a9dad6532d40_d269d60ed8" tabindex="-1" value=""></div>
+					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your e-mail address" autocomplete="off" required /><input type="submit" value="SUBSCRIBE" name="subscribe" class="btn btn-primary" />
+				</form>
+
+				<p class="footer">Copyright © Epic Logger 2015. All rights reserved.</p>
+			</div>
+		</div>
+	</body>
+</html>
