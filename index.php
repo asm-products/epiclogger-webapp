ffa4685faecb47d12a10c7449f517cf17356ca8a
Razvan Ciocanel <chocksy@gmail.com>
2015-05-05 13:29:53 +0300

    code analytics in header

--- index.php
+++ index.php
@@ -14,6 +14,16 @@
 
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-329622-28', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 	</head>
 	<body>
 		<div id="header">
@@ -33,15 +43,5 @@
 				<p class="footer">Copyright © Epic Logger 2015. All rights reserved.</p>
 			</div>
 		</div>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-329622-28', 'auto');
-      ga('send', 'pageview');
-
-    </script>
 	</body>
 </html>
